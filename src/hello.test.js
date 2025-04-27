71bc8f59b586c962ab739c2493315d2bec490170
view <152164438+tanittaya@users.noreply.github.com>
2025-04-27 16:14:01 +0700

    deploy-cicd4

--- src/hello.test.js
+++ src/hello.test.js
@@ -1,12 +1,11 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const chai_1 = require("chai");
-const hello_service_1 = require("./src/services/hello_service");
+import { expect } from "chai";
+import { HelloService } from "./src/services/hello_service"
+
 describe("Test hello world service", () => {
     it("should return hello world", () => {
-        const helloService = new hello_service_1.HelloService();
+        const helloService = new HelloService();
         const result = helloService.sayHello();
-        (0, chai_1.expect)(result).to.equal("Hello, world! from service");
-    });
+
+        expect(result).to.equal("Hello, world! from service");
+    })
 });
-//# sourceMappingURL=hello.test.js.map
